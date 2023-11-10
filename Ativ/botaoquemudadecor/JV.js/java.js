a0e83601b7208f7446b6851aaeeb6a29680a0bc7
BernardoLangmachado <130462210+BernardoLangmachado@users.noreply.github.com>
2023-11-10 11:12:39 -0300

    Add files via upload

---
+++ Ativ/botaoquemudadecor/JV.js/java.js
@@ -0,0 +1,9 @@
+
+function changecolor() {
+    var element = document.body;
+    var element2 = document.getElementById("titulo")
+    var img = document.getElementById("img")
+    img.scr="../image/pngwing.com.png"
+    element.classList.toggle("cordofundo2");
+    element2.classList.toggle("titulo2")
+}
